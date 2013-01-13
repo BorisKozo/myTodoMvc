89ed06bd0c976e331a88e2796272ed23e8692fef
Elad Moshe <elad.moshe@hp.com>
2013-01-13 16:05:19 +0200

    improved code indentation

--- js/views/main_layout_view.js
+++ js/views/main_layout_view.js
@@ -1,52 +1,53 @@
 ﻿/*global define*/
 
 define(['marionette', 'hbs!templates/main_layout', 'js/views/main_header_view', 'js/views/main_content_view', 'js/views/main_footer_view', './../controller'],
-    function (Marionette, layoutTemplate, MainHeaderView, MainContentView, MainFooterView, controller) {
-        'use strict';
-        var SlideAnimationDuration = 200, MainLayoutView = Marionette.Layout.extend({
+   function(Marionette, layoutTemplate, MainHeaderView, MainContentView, MainFooterView, controller) {
+      'use strict';
+      var SlideAnimationDuration = 200,
+         MainLayoutView = Marionette.Layout.extend({
             template: layoutTemplate,
             regions: {
-                header: '#header',
-                content: '#main',
-                footer: '#footer'
+               header: '#header',
+               content: '#main',
+               footer: '#footer'
             },
 
             ui: {
-                'footer': '#footer',
-                'content': '#main'
+               'footer': '#footer',
+               'content': '#main'
             },
 
-            initialize: function () {
-                controller.vent.on('todosUpdated', this.updateFooter, this);
-                this.isDataVisible = false;
+            initialize: function() {
+               controller.vent.on('todosUpdated', this.updateFooter, this);
+               this.isDataVisible = false;
             },
 
-            updateFooter: function (data) {
-                if (data.collection.length === 0 && this.isDataVisible) {
-                    this.ui.footer.hide(SlideAnimationDuration);
-                    this.ui.content.hide(SlideAnimationDuration);
-                    this.isDataVisible = false;
-                    return;
-                }
+            updateFooter: function(data) {
+               if (data.collection.length === 0 && this.isDataVisible) {
+                  this.ui.footer.hide(SlideAnimationDuration);
+                  this.ui.content.hide(SlideAnimationDuration);
+                  this.isDataVisible = false;
+                  return;
+               }
 
-                if (!this.isDataVisible && data.collection.length > 0) {
-                    this.ui.footer.show(SlideAnimationDuration);
-                    this.ui.content.show(SlideAnimationDuration);
-                    this.isDataVisible = true;
-                }
+               if (!this.isDataVisible && data.collection.length > 0) {
+                  this.ui.footer.show(SlideAnimationDuration);
+                  this.ui.content.show(SlideAnimationDuration);
+                  this.isDataVisible = true;
+               }
             },
 
-            onRender: function () {
-                this.header.show(new MainHeaderView());
-                this.content.show(new MainContentView({ collection: this.options.todosCollection }));
-                this.footer.show(new MainFooterView());
+            onRender: function() {
+               this.header.show(new MainHeaderView());
+               this.content.show(new MainContentView({ collection: this.options.todosCollection }));
+               this.footer.show(new MainFooterView());
             },
 
-            onClose: function () {
-                controller.vent.off(null, null, this);
+            onClose: function() {
+               controller.vent.off(null, null, this);
             }
 
-        });
+         });
 
-        return MainLayoutView;
-    });
+      return MainLayoutView;
+   });
