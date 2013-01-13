583eef6958d5141c7f2f8ba9176ea7e49d637d35
Elad Moshe <elad.moshe@hp.com>
2013-01-13 18:39:57 +0200

    refactored saving syntax for clarity

--- js/views/main_content_view.js
+++ js/views/main_content_view.js
@@ -2,86 +2,85 @@
 
 
 define(['backbone', 'marionette', 'underscore', 'hbs!templates/main_content', './todo_item_view', './../models/todo_item_collection', './../controller'],
-    function (Backbone, Marionette, _, contentTemplate, TodoItemView, TodoItemCollection, controller) {
-        'use strict';
-        var ContentView = Marionette.CompositeView.extend({
-            template: contentTemplate,
-            itemViewContainer: '#todo-list',
-            itemView: TodoItemView,
+   function(Backbone, Marionette, _, contentTemplate, TodoItemView, TodoItemCollection, controller) {
+      'use strict';
+      var ContentView = Marionette.CompositeView.extend({
+         template: contentTemplate,
+         itemViewContainer: '#todo-list',
+         itemView: TodoItemView,
 
-            collectionEvents: {
-                'add': 'todoAdded',
-                'remove': 'todoRemoved',
-                'reset': 'collectionLoaded',
-                'finishChanged': 'collectionUpdated'
-            },
+         collectionEvents: {
+            'add': 'todoAdded',
+            'remove': 'todoRemoved',
+            'reset': 'collectionLoaded',
+            'finishChanged': 'collectionUpdated'
+         },
 
-            ui: {
-                'toggleAll': '#toggle-all'
-            },
+         ui: {
+            'toggleAll': '#toggle-all'
+         },
 
-            events: {
-                'click #toggle-all': 'toggleAll'
-            },
+         events: {
+            'click #toggle-all': 'toggleAll'
+         },
 
-            initialize: function () {
-                controller.vent.on('todoTextReady', this.addTodo, this);
-                controller.vent.on('clearCompleted', this.clearCompleted, this);
-                controller.vent.on('todosUpdated', this.todosUpdated, this);
-            },
+         initialize: function() {
+            controller.vent.on('todoTextReady', this.addTodo, this);
+            controller.vent.on('clearCompleted', this.clearCompleted, this);
+            controller.vent.on('todosUpdated', this.todosUpdated, this);
+         },
 
-            addTodo: function (todoText) {
-                this.collection.push({ todoText: todoText });
-            },
+         addTodo: function(todoText) {
+            this.collection.push({ todoText: todoText }).save();
+         },
 
-            todoAdded: function (model, collection) {
-                controller.vent.trigger('todosUpdated', { added: [model], collection: collection });
-                Backbone.sync('create', model);
-            },
+         todoAdded: function(model, collection) {
+            controller.vent.trigger('todosUpdated', { added: [model], collection: collection });
+         },
 
-            todoRemoved: function (model, collection) {
-                controller.vent.trigger('todosUpdated', { removed: [model], collection: collection });
-                Backbone.sync('delete', model);
-            },
+         todoRemoved: function(model, collection) {
+            controller.vent.trigger('todosUpdated', { removed: [model], collection: collection });
+            model.destroy();
+         },
 
-            collectionLoaded: function (collection) {
-                controller.vent.trigger('todosUpdated', { collection: collection });
-            },
+         collectionLoaded: function(collection) {
+            controller.vent.trigger('todosUpdated', { collection: collection });
+         },
 
-            collectionUpdated: function (models, collection) {
-                controller.vent.trigger('todosUpdated', { updated: models, collection: collection });
-                _.each(models, function (model) {
-                    model.save();
-                });
-            },
+         collectionUpdated: function(models, collection) {
+            controller.vent.trigger('todosUpdated', { updated: models, collection: collection });
+            _.each(models, function(model) {
+               model.save();
+            });
+         },
 
-            clearCompleted: function () {
-                var toRemove = this.collection.filter(function (item) {
-                    return item.get('isFinished');
-                });
+         clearCompleted: function() {
+            var toRemove = this.collection.filter(function(item) {
+               return item.get('isFinished');
+            });
 
-                this.collection.remove(toRemove);
-            },
+            this.collection.remove(toRemove);
+         },
 
-            todosUpdated: function (data) {
-                var hasUnfinished = data.collection.some(function (item) {
-                    return !item.get('isFinished');
-                });
+         todosUpdated: function(data) {
+            var hasUnfinished = data.collection.some(function(item) {
+               return !item.get('isFinished');
+            });
 
-                this.ui.toggleAll.prop('checked', !hasUnfinished);
-            },
+            this.ui.toggleAll.prop('checked', !hasUnfinished);
+         },
 
-            toggleAll: function () {
-                this.collection.setFinished(this.ui.toggleAll.is(':checked'));
-            },
+         toggleAll: function() {
+            this.collection.setFinished(this.ui.toggleAll.is(':checked'));
+         },
 
-            onClose: function () {
-                controller.vent.off(null, null, this);
-            }
-
+         onClose: function() {
+            controller.vent.off(null, null, this);
+         }
 
 
-        });
 
-        return ContentView;
-    });
+      });
+
+      return ContentView;
+   });
