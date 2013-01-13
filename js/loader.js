61f9641321c62fa85dd43c721cb086da60f0cb66
Elad Moshe <elad.moshe@hp.com>
2013-01-13 18:42:41 +0200

    added app to main to sure that app is loaded first; added comment to clarify where controllers should be initialized

--- js/loader.js
+++ js/loader.js
@@ -6,7 +6,9 @@ define(['require', './app'],
         var loader = {
             start: function () {
                 require(['./controller', './router'], function (controller) {
-                    //Load and start all the controllers
+
+                    //Load and start *all* the controllers (if there are more controllers implemented you should
+                    // start them all before starting the application.
                     controller.start();
                     App.start();
                 });
